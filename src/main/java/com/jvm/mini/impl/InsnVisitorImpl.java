acd2c0fd93a3c2ee7939da040de6922d5e2ce119
Arno Bastenhof <arnobastenhof@gmail.com>
2015-09-21 17:47:32 +0200

    Replaced auto-generated TODOs with pointers to the JVM SE8 spec.

--- src/main/java/com/jvm/mini/impl/InsnVisitorImpl.java
+++ src/main/java/com/jvm/mini/impl/InsnVisitorImpl.java
@@ -16,212 +16,177 @@ public final class InsnVisitorImpl implements InsnVisitor {
 
 	@Override
 	public void bipush(int operand) {
-		// TODO Auto-generated method stub
-
+		// TODO see https://docs.oracle.com/javase/specs/jvms/se8/html/jvms-6.html#jvms-6.5.bipush
 	}
 
 	@Override
 	public void sipush(int operand) {
-		// TODO Auto-generated method stub
-
+		// TODO see https://docs.oracle.com/javase/specs/jvms/se8/html/jvms-6.html#jvms-6.5.sipush
 	}
 
 	@Override
 	public void ldc(Object constant) {
-		// TODO Auto-generated method stub
-
+		// TODO see https://docs.oracle.com/javase/specs/jvms/se8/html/jvms-6.html#jvms-6.5.ldc
 	}
 
 	@Override
 	public void i2b() {
-		// TODO Auto-generated method stub
-
+		// TODO see https://docs.oracle.com/javase/specs/jvms/se8/html/jvms-6.html#jvms-6.5.i2b
 	}
 
 	@Override
 	public void i2s() {
-		// TODO Auto-generated method stub
-
+		// TODO see https://docs.oracle.com/javase/specs/jvms/se8/html/jvms-6.html#jvms-6.5.i2s
 	}
 
 	@Override
 	public void i2l() {
-		// TODO Auto-generated method stub
-
+		// TODO see https://docs.oracle.com/javase/specs/jvms/se8/html/jvms-6.html#jvms-6.5.i2l
 	}
 
 	@Override
 	public void l2i() {
-		// TODO Auto-generated method stub
-
+		// TODO see https://docs.oracle.com/javase/specs/jvms/se8/html/jvms-6.html#jvms-6.5.l2i
 	}
 
 	@Override
 	public void iadd() {
-		// TODO Auto-generated method stub
-
+		// TODO see https://docs.oracle.com/javase/specs/jvms/se8/html/jvms-6.html#jvms-6.5.iadd
 	}
 
 	@Override
 	public void ladd() {
-		// TODO Auto-generated method stub
-
+		// TODO see https://docs.oracle.com/javase/specs/jvms/se8/html/jvms-6.html#jvms-6.5.ladd
 	}
 
 	@Override
 	public void isub() {
-		// TODO Auto-generated method stub
-
+		// TODO see https://docs.oracle.com/javase/specs/jvms/se8/html/jvms-6.html#jvms-6.5.isub
 	}
 
 	@Override
 	public void lsub() {
-		// TODO Auto-generated method stub
-
+		// TODO see https://docs.oracle.com/javase/specs/jvms/se8/html/jvms-6.html#jvms-6.5.lsub
 	}
 
 	@Override
 	public void imul() {
-		// TODO Auto-generated method stub
-
+		// TODO see https://docs.oracle.com/javase/specs/jvms/se8/html/jvms-6.html#jvms-6.5.imul
 	}
 
 	@Override
 	public void lmul() {
-		// TODO Auto-generated method stub
-
+		// TODO https://docs.oracle.com/javase/specs/jvms/se8/html/jvms-6.html#jvms-6.5.lmul
 	}
 
 	@Override
 	public void ineg() {
-		// TODO Auto-generated method stub
-
+		// TODO https://docs.oracle.com/javase/specs/jvms/se8/html/jvms-6.html#jvms-6.5.ineg
 	}
 
 	@Override
 	public void lneg() {
-		// TODO Auto-generated method stub
-
+		// TODO see https://docs.oracle.com/javase/specs/jvms/se8/html/jvms-6.html#jvms-6.5.lneg
 	}
 
 	@Override
 	public void iload(int var) {
-		// TODO Auto-generated method stub
-
+		// TODO see https://docs.oracle.com/javase/specs/jvms/se8/html/jvms-6.html#jvms-6.5.iload
 	}
 
 	@Override
 	public void lload(int var) {
-		// TODO Auto-generated method stub
-
+		// TODO see https://docs.oracle.com/javase/specs/jvms/se8/html/jvms-6.html#jvms-6.5.lload
 	}
 
 	@Override
 	public void istore(int var) {
-		// TODO Auto-generated method stub
-
+		// TODO see https://docs.oracle.com/javase/specs/jvms/se8/html/jvms-6.html#jvms-6.5.istore
 	}
 
 	@Override
 	public void lstore(int var) {
-		// TODO Auto-generated method stub
-
+		// TODO see https://docs.oracle.com/javase/specs/jvms/se8/html/jvms-6.html#jvms-6.5.lstore
 	}
 
 	@Override
 	public void iinc(int var, int increment) {
-		// TODO Auto-generated method stub
-
+		// TODO see https://docs.oracle.com/javase/specs/jvms/se8/html/jvms-6.html#jvms-6.5.iinc
 	}
 
 	@Override
 	public void lcmp() {
-		// TODO Auto-generated method stub
-
+		// TODO see https://docs.oracle.com/javase/specs/jvms/se8/html/jvms-6.html#jvms-6.5.lcmp
 	}
 
 	@Override
 	public void ifIcmpeq(AbstractInsnNode jumpTarget) {
-		// TODO Auto-generated method stub
-
+		// TODO see https://docs.oracle.com/javase/specs/jvms/se8/html/jvms-6.html#jvms-6.5.if_icmp_cond
 	}
 
 	@Override
 	public void ifIcmpne(AbstractInsnNode jumpTarget) {
-		// TODO Auto-generated method stub
-
+		// TODO see https://docs.oracle.com/javase/specs/jvms/se8/html/jvms-6.html#jvms-6.5.if_icmp_cond
 	}
 
 	@Override
 	public void ifIcmplt(AbstractInsnNode jumpTarget) {
-		// TODO Auto-generated method stub
-
+		// TODO see https://docs.oracle.com/javase/specs/jvms/se8/html/jvms-6.html#jvms-6.5.if_icmp_cond
 	}
 
 	@Override
 	public void ifIcmple(AbstractInsnNode jumpTarget) {
-		// TODO Auto-generated method stub
-
+		// TODO see https://docs.oracle.com/javase/specs/jvms/se8/html/jvms-6.html#jvms-6.5.if_icmp_cond
 	}
 
 	@Override
 	public void ifIcmpgt(AbstractInsnNode jumpTarget) {
-		// TODO Auto-generated method stub
-
+		// TODO see https://docs.oracle.com/javase/specs/jvms/se8/html/jvms-6.html#jvms-6.5.if_icmp_cond
 	}
 
 	@Override
 	public void ifIcmpge(AbstractInsnNode jumpTarget) {
-		// TODO Auto-generated method stub
-
+		// TODO see https://docs.oracle.com/javase/specs/jvms/se8/html/jvms-6.html#jvms-6.5.if_icmp_cond
 	}
 
 	@Override
 	public void ifEq(AbstractInsnNode jumpTarget) {
-		// TODO Auto-generated method stub
-
+		// TODO see https://docs.oracle.com/javase/specs/jvms/se8/html/jvms-6.html#jvms-6.5.if_cond
 	}
 
 	@Override
 	public void ifNe(AbstractInsnNode jumpTarget) {
-		// TODO Auto-generated method stub
-
+		// TODO see https://docs.oracle.com/javase/specs/jvms/se8/html/jvms-6.html#jvms-6.5.if_cond
 	}
 
 	@Override
 	public void ifLt(AbstractInsnNode jumpTarget) {
-		// TODO Auto-generated method stub
-
+		// TODO see https://docs.oracle.com/javase/specs/jvms/se8/html/jvms-6.html#jvms-6.5.if_cond
 	}
 
 	@Override
 	public void ifLe(AbstractInsnNode jumpTarget) {
-		// TODO Auto-generated method stub
-
+		// TODO see https://docs.oracle.com/javase/specs/jvms/se8/html/jvms-6.html#jvms-6.5.if_cond
 	}
 
 	@Override
 	public void ifGt(AbstractInsnNode jumpTarget) {
-		// TODO Auto-generated method stub
-
+		// TODO see https://docs.oracle.com/javase/specs/jvms/se8/html/jvms-6.html#jvms-6.5.if_cond
 	}
 
 	@Override
 	public void ifGe(AbstractInsnNode jumpTarget) {
-		// TODO Auto-generated method stub
-
+		// TODO see https://docs.oracle.com/javase/specs/jvms/se8/html/jvms-6.html#jvms-6.5.if_cond
 	}
 
 	@Override
 	public void goTo(AbstractInsnNode jumpTarget) {
-		// TODO Auto-generated method stub
-
+		// TODO see https://docs.oracle.com/javase/specs/jvms/se8/html/jvms-6.html#jvms-6.5.goto
 	}
 
 	@Override
 	public void voidReturn() {
-		// TODO Auto-generated method stub
-
+		// TODO see https://docs.oracle.com/javase/specs/jvms/se8/html/jvms-6.html#jvms-6.5.return
 	}
 
 }
